07e5e435536123124be52a6ac8f2122a2fc18f08
moulikori <109732285+moulikori@users.noreply.github.com>
2022-07-21 16:00:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is my first angular project
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-5ttqyy)
---
+++ src/app/colorfull/colorfull.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'colorfull',
+  templateUrl: './colorfull.component.html',
+  styleUrls: ['./colorfull.component.css'],
+})
+export class ColorfullComponent {
+  numberOfLikes: number;
+}
